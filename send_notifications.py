07ee6c01ac214d4309e3530a3e79ef61d6f1a554
newmad4 <khimich.vv@uds.ua.energy>
2022-07-15 20:00:31 +0300

    use multithereding in reason of I/O operations aspecially for a lot of notifications;

--- send_notifications.py
+++ send_notifications.py
@@ -1,9 +1,16 @@
+from concurrent.futures import ThreadPoolExecutor
+
 from services import NotificationService
 from utils import fetch_notifications_data
 
+
 notificator = NotificationService()
 
-for data in fetch_notifications_data():
-    notificator.send(data)
+with ThreadPoolExecutor() as executor:
+    for data in fetch_notifications_data():
+        executor.submit(
+            notificator.send,
+            data
+        )
 
 notificator.log_success_results()
