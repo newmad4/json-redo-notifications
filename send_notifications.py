c24f32eab0bb02dfe2266863f79127f167f5b119
newmad4 <khimich.vv@uds.ua.energy>
2022-07-15 19:02:03 +0300

    Add notification factory and registrate all notification services;

--- send_notifications.py
+++ send_notifications.py
@@ -96,3 +96,26 @@ class PostService(AbstractNotificationService):
 
     def send(self, url: str, data: dict) -> None:
         print(f"POST sent to {url}. Data: {data}")
+
+
+class NotificationFactory:
+    """Factory class for registration and get service by notification type."""
+
+    def __init__(self):
+        self._services = {}
+
+    def register_type(self, notification_type, service) -> None:
+        self._services[notification_type] = service
+
+    def get_service(self, notification_type):
+        service = self._services.get(notification_type)
+        if not service:
+            print("Unprocessed notification type!")
+            return None
+        return service()
+
+
+factory = NotificationFactory()
+factory.register_type("post", PostService)
+factory.register_type("email", EmailService)
+factory.register_type("sms", SMSService)
