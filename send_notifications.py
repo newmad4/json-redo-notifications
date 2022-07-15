9cec0fa4c16716e638af2840c85f6f1fbb0800bf
newmad4 <khimich.vv@uds.ua.energy>
2022-07-15 18:20:34 +0300

    Add basic functions for send different notifications; try to send notifications.

--- send_notifications.py
+++ send_notifications.py
@@ -7,4 +7,27 @@ def fetch_notifications_data() -> list:
     response = requests.get(REDO_DATA_SOURCE_URL)
     return response.json()
 
-print(fetch_notifications_data())
+
+def send_sms(phone: str, data: dict) -> None:
+    print(f"SMS sent to {phone}. Data: {data}")
+
+
+def send_email(email: str, data: dict) -> None:
+    print(f"EMAIL sent to {email}. Data: {data}")
+
+
+def send_post(url: str, data: dict) -> None:
+    print(f"POST sent to {url}. Data: {data}")
+
+
+for data in fetch_notifications_data():
+    notification_type = data["type"]
+
+    if notification_type == "sms":
+        send_sms(data["phone"], data)
+    elif notification_type == "email":
+        send_email(data["email"], data)
+    elif notification_type == "post":
+        send_email(data["url"], data)
+    else:
+        print("Unprocessed notification type!")
