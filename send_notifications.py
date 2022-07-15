2e79a05e5a685a7bdd37482b8dbafd2b94e3d4f2
newmad4 <khimich.vv@uds.ua.energy>
2022-07-15 18:47:00 +0300

    Write sent notifications to result JSON file

--- send_notifications.py
+++ send_notifications.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 
 REDO_DATA_SOURCE_URL = "https://raw.githubusercontent.com/UN-ICC/notifications-processor/master/notifications_log.json"
@@ -32,6 +34,7 @@ def is_valid_data(notification_type: str, data: dict) -> bool:
     return True
 
 
+processed_data = list()
 for data in fetch_notifications_data():
     notification_type = data["type"]
 
@@ -39,6 +42,7 @@ for data in fetch_notifications_data():
         print(f"Invalid notification data: {data}")
         continue
 
+    notification_was_sent = True
     if notification_type == "sms":
         send_sms(data["phone"], data)
     elif notification_type == "email":
@@ -46,4 +50,11 @@ for data in fetch_notifications_data():
     elif notification_type == "post":
         send_email(data["url"], data)
     else:
+        notification_was_sent = False
         print("Unprocessed notification type!")
+
+    if notification_was_sent:
+        processed_data.append(data)
+
+with open("processed_notifications.json", "w") as f:
+    json.dump(processed_data, f, indent=2)
