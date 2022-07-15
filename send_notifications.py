e82bf6bfbe4f5ee646552f93a352939983302d8a
newmad4 <khimich.vv@uds.ua.energy>
2022-07-15 18:30:44 +0300

    Add validation for mandatory fields;

--- send_notifications.py
+++ send_notifications.py
@@ -1,6 +1,11 @@
 import requests
 
 REDO_DATA_SOURCE_URL = "https://raw.githubusercontent.com/UN-ICC/notifications-processor/master/notifications_log.json"
+MANDATORY_FIELDS = {
+    "sms": ("phone", "name"),
+    "email": ("email", "name"),
+    "post": ("url", "name"),
+}
 
 
 def fetch_notifications_data() -> list:
@@ -20,8 +25,19 @@ def send_post(url: str, data: dict) -> None:
     print(f"POST sent to {url}. Data: {data}")
 
 
+def is_valid_data(notification_type: str, data: dict) -> bool:
+    for parameter_name in MANDATORY_FIELDS.get(notification_type):
+        if not data.get(parameter_name):
+            return False
+    return True
+
+
 for data in fetch_notifications_data():
     notification_type = data["type"]
+
+    if not is_valid_data(notification_type, data):
+        print(f"Invalid notification data: {data}")
+        continue
 
     if notification_type == "sms":
         send_sms(data["phone"], data)
